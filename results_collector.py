d7c4f5c3db15c7bbd506207cbf27d80ff5f7e665
Joseph Giovanelli <joseph.giovanelli@studio.unibo.it>
2019-10-10 14:25:32 +0200

    Add pipeline in the result

--- results_collector.py
+++ results_collector.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 
 import argparse
+import collections
 import os
 from os import listdir
 from os.path import isfile, join
@@ -36,29 +37,35 @@ for path in input_paths:
     for result in results:
         with open(os.path.join(path, result)) as json_file:
             data = json.load(json_file)
-            comparison[path][result[:-5]] = data['context']['max_history_score']
+            accuracy = data['context']['max_history_score']
+            pipeline = str(data['context']['best_config']['pipeline']).replace(",", " ")
+            comparison[path][result[:-5]] = (accuracy, pipeline)
 
-results = mergeDict(comparison[input_paths[0]], comparison[input_paths[1]])
+results = collections.OrderedDict(sorted(mergeDict(comparison[input_paths[0]], comparison[input_paths[1]]).items()))
 partial_results = {}
 
 for algorithm in algorithms:
     acronym = ''.join([a for a in algorithm if a.isupper()]).lower()
     partial_results[acronym] = {'conf1': 0, 'draws': 0, 'conf2': 0}
     with open(os.path.join(result_path, '{}.csv'.format(acronym)), "a") as out:
-        out.write("dataset,accuracy1,accuracy2\n")
+        out.write("dataset,conf1,pipeline1,conf2,pipeline2\n")
 
 for key, value in results.items():
     acronym = key.split("_")[0]
     dataset = key.split("_")[1]
-    partial_results[acronym]['conf1'] += 1 if value[0] - value[1] >= 0.001 else 0
-    partial_results[acronym]['draws'] += 1 if (value[0] - value[1] <= 0.001) and (value[1] - value[0] <= 0.001) else 0
-    partial_results[acronym]['conf2'] += 1 if value[1] - value[0] >= 0.001 else 0
+    conf1 = value[0][0]
+    conf2 = value[1][0]
+    pipeline1 = value[0][1]
+    pipeline2 = value[1][1]
+    partial_results[acronym]['conf1'] += 1 if conf1 - conf2 >= 0.001 else 0
+    partial_results[acronym]['draws'] += 1 if (conf1 - conf2 <= 0.001) and (conf2 - conf1 <= 0.001) else 0
+    partial_results[acronym]['conf2'] += 1 if conf2 - conf1 >= 0.001 else 0
     with open(os.path.join(result_path, '{}.csv'.format(acronym)), "a") as out:
-        out.write(dataset + "," + str(value[0]) + "," + str(value[1]) + "\n")
+        out.write(dataset + "," + str(conf1) + "," + str(pipeline1) + "," + str(conf2) + "," + str(pipeline2) + "\n")
 
-complete_results = {'conf1': sum(value[0] - value[1] >= 0.001 for value in results.values()),
-                 'draws': sum((value[0] - value[1] <= 0.001) and (value[1] - value[0] <= 0.001) for value in results.values()),
-                 'conf2': sum(value[1] - value[0] >= 0.001 for value in results.values())}
+complete_results = {'conf1': sum(value[0][0] - value[1][0] >= 0.001 for value in results.values()),
+                 'draws': sum((value[0][0] - value[1][0] <= 0.001) and (value[1][0] - value[0][0] <= 0.001) for value in results.values()),
+                 'conf2': sum(value[1][0] - value[0][0] >= 0.001 for value in results.values())}
 
 with open(os.path.join(result_path, 'results.txt'), "a") as out:
     out.write("GROUPED BY THE ALGORITHMS")
