7e396677db7ed193392c221b08a7b98ae543458e
Joseph Giovanelli <joseph.giovanelli@studio.unibo.it>
2019-10-25 15:42:53 +0200

    Update wrapper_experiments.sh

--- wrapper_experiments.sh
+++ wrapper_experiments.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python3.7 experiments_launcher.py -p features rebalance -r results/features_rebalance/conf1
-python3.7 experiments_launcher.py -p rebalance features -r results/features_rebalance/conf2
-python3.7 results_collector.py -i results/features_rebalance/conf1 -ii results/features_rebalance/conf2 -o results/features_rebalance
+python3 experiments_launcher.py -p features rebalance -r results/features_rebalance/conf1
+python3 experiments_launcher.py -p rebalance features -r results/features_rebalance/conf2
+python3 results_collector.py -p features rebalance -i results/features_rebalance/conf1 -ii results/features_rebalance/conf2 -o results/features_rebalance
