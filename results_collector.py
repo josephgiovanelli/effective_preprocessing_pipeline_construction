dfc747bbb6021571d2cedead2c2306e5b310e1c2
Joseph Giovanelli <joseph.giovanelli@studio.unibo.it>
2019-10-26 16:29:20 +0200

    Update results_collector.py

--- results_collector.py
+++ results_collector.py
@@ -5,6 +5,8 @@ from yellowbrick.target import FeatureCorrelation
 from os import listdir
 from os.path import isfile, join
 from sklearn.preprocessing import OrdinalEncoder
+from scipy import stats as s
+
 
 import argparse
 import collections
@@ -83,10 +85,13 @@ def create_output_files(result_path, grouped_by_algorithm_results, scheme):
     first = scheme[0][0].upper()
     second = scheme[1][0].upper()
     firstOrSecond = first + "o" + second
+    firstSecond = first + second
+    secondFirst = second + first
+    draws = firstSecond + "o" + secondFirst
 
     for algorithm in algorithms:
         acronym = ''.join([a for a in algorithm if a.isupper()]).lower()
-        grouped_by_algorithm_results[acronym] = {'conf1': 0, 'draws': 0, 'conf2': 0, 'baseline': 0, first: 0, second: 0, firstOrSecond: 0,'valid': 0}
+        grouped_by_algorithm_results[acronym] = {firstSecond: 0, draws: 0, secondFirst: 0, 'NN': 0, first: 0, second: 0, firstOrSecond: 0, 'valid': 0}
         with open(os.path.join(result_path, '{}.csv'.format(acronym)), "a") as out:
             out.write("dataset,name,dimensions,conf1,pipeline1,num_iterations1,best_iteration1,conf2,pipeline2,"
                       "num_iterations2,best_iteration2,valid\n")
@@ -124,23 +129,26 @@ def compute_result(result, dataset, acronym, grouped_by_algorithm_results, group
     first = scheme[0][0].upper()
     second = scheme[1][0].upper()
     firstOrSecond = first + "o" + second
+    firstSecond = first + second
+    secondFirst = second + first
+    draws = firstSecond + "o" + secondFirst
 
     #case a, b, c, e, i
     if result == 0 and baseline_scores[0] == scores[0]:
-        grouped_by_dataset_result[dataset][acronym] = 'baseline'
-        grouped_by_algorithm_results[acronym]['baseline'] += 1
+        grouped_by_dataset_result[dataset][acronym] = 'NN'
+        grouped_by_algorithm_results[acronym]['NN'] += 1
     #case d, o
     elif pipelines["pipeline1"].count('NoneType') == 2 or pipelines["pipeline2"].count('NoneType') == 2:
         if pipelines["pipeline1"].count('NoneType') == 2:
             if result == 2:
-                grouped_by_dataset_result[dataset][acronym] = 'conf2'
-                grouped_by_algorithm_results[acronym]['conf2'] += 1
+                grouped_by_dataset_result[dataset][acronym] = secondFirst
+                grouped_by_algorithm_results[acronym][secondFirst] += 1
             else:
                 print("pipeline2 is not winning. " + str(pipelines) + " baseline_score " + str(baseline_scores[0]) + " scores " + str(scores) + " algorithm " + str(acronym))
         else:
             if result == 1:
-                grouped_by_dataset_result[dataset][acronym] = 'conf1'
-                grouped_by_algorithm_results[acronym]['conf1'] += 1
+                grouped_by_dataset_result[dataset][acronym] = firstSecond
+                grouped_by_algorithm_results[acronym][firstSecond] += 1
             else:
                 print("pipeline1 is not winning. " + str(pipelines) + " baseline_score " + str(baseline_scores[0]) + " scores " + str(scores) + " algorithm " + str(acronym))
     #case f, m, l, g
@@ -174,8 +182,8 @@ def compute_result(result, dataset, acronym, grouped_by_algorithm_results, group
                 grouped_by_dataset_result[dataset][acronym] = second
                 grouped_by_algorithm_results[acronym][second] += 1
             elif result == 2:
-                grouped_by_dataset_result[dataset][acronym] = 'conf2'
-                grouped_by_algorithm_results[acronym]['conf2'] += 1
+                grouped_by_dataset_result[dataset][acronym] = secondFirst
+                grouped_by_algorithm_results[acronym][secondFirst] += 1
             else:
                 print("pipeline2 is not winning. " + str(pipelines) + " baseline_score " + str(baseline_scores[0]) + " scores " + str(scores) + " algorithm " + str(acronym))
         #case n
@@ -184,8 +192,8 @@ def compute_result(result, dataset, acronym, grouped_by_algorithm_results, group
                 grouped_by_dataset_result[dataset][acronym] = first
                 grouped_by_algorithm_results[acronym][first] += 1
             elif result == 2:
-                grouped_by_dataset_result[dataset][acronym] = 'conf2'
-                grouped_by_algorithm_results[acronym]['conf2'] += 1
+                grouped_by_dataset_result[dataset][acronym] = secondFirst
+                grouped_by_algorithm_results[acronym][secondFirst] += 1
             else:
                 print("pipeline2 is not winning. " + str(pipelines) + " baseline_score " + str(baseline_scores[0]) + " scores " + str(scores) + " algorithm " + str(acronym))
     # case p, q
@@ -196,8 +204,8 @@ def compute_result(result, dataset, acronym, grouped_by_algorithm_results, group
                 grouped_by_dataset_result[dataset][acronym] = second
                 grouped_by_algorithm_results[acronym][second] += 1
             elif result == 1:
-                grouped_by_dataset_result[dataset][acronym] = 'conf1'
-                grouped_by_algorithm_results[acronym]['conf1'] += 1
+                grouped_by_dataset_result[dataset][acronym] = firstSecond
+                grouped_by_algorithm_results[acronym][firstSecond] += 1
             else:
                 print("pipeline1 is not winning. " + str(pipelines) + " baseline_score " + str(baseline_scores[0]) + " scores " + str(scores) + " algorithm " + str(acronym))
         # case q
@@ -206,21 +214,21 @@ def compute_result(result, dataset, acronym, grouped_by_algorithm_results, group
                 grouped_by_dataset_result[dataset][acronym] = second
                 grouped_by_algorithm_results[acronym][second] += 1
             elif result == 1:
-                grouped_by_dataset_result[dataset][acronym] = 'conf1'
-                grouped_by_algorithm_results[acronym]['conf1'] += 1
+                grouped_by_dataset_result[dataset][acronym] = firstSecond
+                grouped_by_algorithm_results[acronym][firstSecond] += 1
             else:
                 print("pipeline1 is not winning. " + str(pipelines) + " baseline_score " + str(baseline_scores[0]) + " scores " + str(scores) + " algorithm " + str(acronym))
     #case r
     elif pipelines["pipeline1"].count('NoneType') == 0 and pipelines["pipeline2"].count('NoneType') == 0:
         if result == 0:
-            grouped_by_dataset_result[dataset][acronym] = 'draws'
-            grouped_by_algorithm_results[acronym]['draws'] += 1
+            grouped_by_dataset_result[dataset][acronym] = draws
+            grouped_by_algorithm_results[acronym][draws] += 1
         elif result == 1:
-            grouped_by_dataset_result[dataset][acronym] = 'conf1'
-            grouped_by_algorithm_results[acronym]['conf1'] += 1
+            grouped_by_dataset_result[dataset][acronym] = firstSecond
+            grouped_by_algorithm_results[acronym][firstSecond] += 1
         elif result == 2:
-            grouped_by_dataset_result[dataset][acronym] = 'conf2'
-            grouped_by_algorithm_results[acronym]['conf2'] += 1
+            grouped_by_dataset_result[dataset][acronym] = secondFirst
+            grouped_by_algorithm_results[acronym][secondFirst] += 1
     else:
         print("This configuration matches nothing. " + str(pipelines) + " baseline_score " + str(baseline_scores[0]) + " scores " + str(scores) + " algorithm " + str(acronym))
 
@@ -232,6 +240,11 @@ def aggregate_results(filtered_datasets, results, grouped_by_algorithm_results, 
     df = df.loc[df['did'].isin(filtered_datasets)]
     first = pipeline[0][0].upper()
     second = pipeline[1][0].upper()
+    firstOrSecond = first + "o" + second
+    firstSecond = first + second
+    secondFirst = second + first
+    draws = firstSecond + "o" + secondFirst
+
     firstOrSecond = first + "o" + second
     grouped_by_dataset_result = {}
     for key, value in results.items():
@@ -267,10 +280,10 @@ def aggregate_results(filtered_datasets, results, grouped_by_algorithm_results, 
                       str(value[0][2]) + "," + str(value[0][3]) + "," + str(conf2) + "," + str(value[1][1]) +
                       "," + str(value[1][2]) +  "," + str(value[1][3]) +  "," + str(valid) + "\n")
 
-    summary = {'conf1': sum(x['conf1'] for x in grouped_by_algorithm_results.values()),
-                     'draws': sum(x['draws'] for x in grouped_by_algorithm_results.values()),
-                     'conf2': sum(x['conf2'] for x in grouped_by_algorithm_results.values()),
-                     'baseline': sum(x['baseline'] for x in grouped_by_algorithm_results.values()),
+    summary = {firstSecond: sum(x[firstSecond] for x in grouped_by_algorithm_results.values()),
+                     draws: sum(x[draws] for x in grouped_by_algorithm_results.values()),
+                     secondFirst: sum(x[secondFirst] for x in grouped_by_algorithm_results.values()),
+                     'NN': sum(x['NN'] for x in grouped_by_algorithm_results.values()),
                      first: sum(x[first] for x in grouped_by_algorithm_results.values()),
                      second: sum(x[second] for x in grouped_by_algorithm_results.values()),
                      firstOrSecond: sum(x[firstOrSecond] for x in grouped_by_algorithm_results.values()),
@@ -281,8 +294,11 @@ def write_summary(result_path, grouped_by_algorithm_results, summary, scheme):
     first = scheme[0][0].upper()
     second = scheme[1][0].upper()
     firstOrSecond = first + "o" + second
+    firstSecond = first + second
+    secondFirst = second + first
+    draws = firstSecond + "o" + secondFirst
     with open(os.path.join(result_path, 'results.csv'), "a") as out:
-        out.write("algorithm,conf1,draws,conf2,baseline," + first + "," + second + "," + firstOrSecond + ",valid\n")
+        out.write("algorithm," + firstSecond + "," + draws + "," + secondFirst + ",NN," + first + "," + second + "," + firstOrSecond + ",valid\n")
         for key, value in grouped_by_algorithm_results.items():
             row = key
             for k, v in value.items():
@@ -329,29 +345,62 @@ def create_num_equal_elements_matrix(result_path, grouped_by_dataset_result):
             out.write(row)
 
 
-def get_correlation_matrix(result_path, grouped_by_dataset_result):
+def create_hamming_matrix(result_path, X, y):
+    def hamming_distance(s1, s2):
+        assert len(s1) == len(s2)
+        return sum(ch1 != ch2 for ch1, ch2 in zip(s1, s2))
+    hamming_matrix = np.zeros((5, 5))
+    value = np.zeros(5)
+    value[X[0][1]] = y[0] + 1
+    for i in range(1, np.size(X, 0)):
+        if X[i][0] == X[i-1][0]:
+            value[X[i][1]] = y[i] + 1
+        else:
+            most_frequent = int(s.mode([x for x in value if x != 0])[0])
+            weight = list(value).count(0)
+            ideal = np.zeros(5)
+            for j in range(0, np.size(value, 0)):
+                if value[j] != 0:
+                    ideal[j] = most_frequent
+            hamming_matrix[weight][hamming_distance(value, ideal)] += 1
+
+            value = np.zeros(5)
+            value[X[i][1]] = y[i] + 1
+    print(hamming_matrix)
+
+
+
+def create_correlation_matrix(result_path, grouped_by_dataset_result):
     data = []
     for dataset, value in grouped_by_dataset_result.items():
         for algorithm, result in value.items():
             data.append([dataset, algorithm, result])
 
     df = pd.DataFrame(OrdinalEncoder().fit_transform(data))
-    print(df.corr(method='kendall'))
-    print(df.corr(method='pearson'))
-    print(df.corr(method='spearman'))
+    kendall = df.corr(method='kendall')
+    pearson = df.corr(method='pearson')
+    spearman = df.corr(method='spearman')
 
     f = np.array(['dataset', 'algorithm'])
     data = OrdinalEncoder().fit_transform(data)
     X = data[:,:-1]
     y = data[:,-1]
+    create_hamming_matrix(result_path, X.astype(int), y.astype(int))
     visualizer = FeatureCorrelation(method='mutual_info-regression', labels=f)
     visualizer.fit(X, y, discrete_features=[True, True], random_state=0)
-    print(visualizer.scores_)
+    mutual_info_regression = visualizer.scores_
 
     estimator = SVR(kernel="linear")
     selector = RFE(estimator, step=1, n_features_to_select=2)
     selector = selector.fit(X, y)
-    print(selector.estimator_.coef_)
+    rfe_coefficient = selector.estimator_.coef_
+
+    with open(os.path.join(result_path, 'correlation_matrix.csv'), "a") as out:
+        out.write(",kendall,pearson,spearman,mutual_info_regression,rfe_coefficient\n")
+        out.write("dataset," + str(kendall[0][2]) + "," + str(pearson[0][2]) + "," + str(spearman[0][2]) + "," +
+                  str(mutual_info_regression[0]) + "," + str(rfe_coefficient[0][0]) + "\n")
+        out.write("algorithm," + str(kendall[1][2]) + "," + str(pearson[1][2]) + "," + str(spearman[1][2]) + "," +
+                  str(mutual_info_regression[1]) + "," + str(rfe_coefficient[0][1]))
 
 def main():
     input_paths, result_path, pipeline = parse_args()
@@ -362,8 +411,7 @@ def main():
         filtered_datasets, results, grouped_by_algorithm_results, result_path, pipeline)
     write_summary(result_path, grouped_by_algorithm_results, summary, pipeline)
     create_num_equal_elements_matrix(result_path, grouped_by_dataset_result)
-    get_correlation_matrix(result_path, grouped_by_dataset_result)
-
+    create_correlation_matrix(result_path, grouped_by_dataset_result)
 
 main()
 
