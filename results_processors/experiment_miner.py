40fb34c9b003fa587b806eadbc0b4d7c3ce77369
Joseph Giovanelli <joseph.giovanelli@studio.unibo.it>
2019-10-31 12:58:48 +0100

    Update experiment_miner.py

--- results_processors/experiment_miner.py
+++ results_processors/experiment_miner.py
@@ -21,7 +21,7 @@ benchmark_suite = [3, 6, 11, 12, 14, 15, 16, 18, 22, 23, 28, 29, 31, 32, 37, 44,
                        40668, 40966, 40982, 40994, 40983, 40975, 40984, 40979, 40996, 41027, 23517, 40923, 40927, 40978,
                        40670, 40701]
 
-def mergeDict(dict1, dict2):
+def merge_dict(dict1, dict2):
     """ Merge dictionaries and keep values of common keys in list"""
     dict3 = {**dict2, **dict1}
     for key, value in dict3.items():
@@ -92,21 +92,27 @@ def load_results(input_paths, filtered_datasets):
                         num_iterations = data['context']['iteration'] + 1
                         best_iteration = data['context']['best_config']['iteration'] + 1
                         baseline_score = data['context']['baseline_score'] // 0.0001 / 100
-                        comparison[path][acronym] = (accuracy, pipeline, num_iterations, best_iteration, baseline_score)
                 else:
                     accuracy = 0
                     pipeline = ""
                     num_iterations = 0
                     best_iteration = 0
                     baseline_score = 0
-                    comparison[path][acronym] = (accuracy, pipeline, num_iterations, best_iteration, baseline_score)
+                comparison[path][acronym] = {}
+                comparison[path][acronym]['accuracy'] = accuracy
+                comparison[path][acronym]['pipeline'] = pipeline
+                comparison[path][acronym]['num_iterations'] = num_iterations
+                comparison[path][acronym]['best_iteration'] = best_iteration
+                comparison[path][acronym]['baseline_score'] = baseline_score
 
-    return collections.OrderedDict(sorted(mergeDict(comparison[input_paths[0]], comparison[input_paths[1]]).items()))
+    return collections.OrderedDict(sorted(merge_dict(comparison[input_paths[0]], comparison[input_paths[1]]).items()))
 
 
 def write_simple_results(result_path, simple_results, filtered_datasets):
     for algorithm in algorithms:
         acronym = ''.join([a for a in algorithm if a.isupper()]).lower()
+        if os.path.exists('{}.csv'.format(acronym)):
+            os.remove('{}.csv'.format(acronym))
         with open(os.path.join(result_path, '{}.csv'.format(acronym)), "a") as out:
             out.write("dataset,name,dimensions,conf1,pipeline1,num_iterations1,best_iteration1,conf2,pipeline2,"
                       "num_iterations2,best_iteration2\n")
@@ -117,16 +123,17 @@ def write_simple_results(result_path, simple_results, filtered_datasets):
     for key, value in simple_results.items():
         acronym = key.split("_")[0]
         dataset = key.split("_")[1]
-        conf1 = value[0][0]
-        conf2 = value[1][0]
         name = df.loc[df['did'] == int(dataset)]['name'].values.tolist()[0]
         dimensions = ' x '.join([str(int(a)) for a in df.loc[df['did'] == int(dataset)][
             ['NumberOfInstances', 'NumberOfFeatures']].values.flatten().tolist()])
 
         with open(os.path.join(result_path, '{}.csv'.format(acronym)), "a") as out:
-            out.write(dataset + "," + name + "," + dimensions + "," + str(conf1) + "," + str(value[0][1]) + "," +
-                      str(value[0][2]) + "," + str(value[0][3]) + "," + str(conf2) + "," + str(value[1][1]) +
-                      "," + str(value[1][2]) + "," + str(value[1][3]) + "\n")
+            out.write(dataset + "," + name + "," + dimensions + "," + str(value[0]['accuracy']) + "," +
+                      str(value[0]['pipeline']) + "," + str(value[0]['num_iterations']) + "," +
+                      str(value[0]['best_iteration']) + ","+ str(value[0]['accuracy']) + "," +
+                      str(value[1]['pipeline']) + "," + str(value[1]['num_iterations']) + "," +
+                      str(value[1]['best_iteration']) + "\n")
+
 
 def compose_pipeline(pipeline1, pipeline2, scheme):
     pipelines = {"pipeline1": [], "pipeline2": []}
@@ -141,7 +148,6 @@ def compose_pipeline(pipeline1, pipeline2, scheme):
 
 
 def check_validity(pipelines, result):
-    validity = False
     if pipelines["pipeline1"] == [] and pipelines["pipeline2"] == []:
         validity, problem = False, 'not_exec'
     elif pipelines["pipeline1"] == [] or pipelines["pipeline2"] == []:
@@ -271,15 +277,13 @@ def aggregate_results(simple_results, pipeline, categories):
     for key, value in simple_results.items():
         acronym = key.split("_")[0]
         dataset = key.split("_")[1]
-        conf1 = value[0][0]
-        conf2 = value[1][0]
-        pipelines = compose_pipeline(value[0][1], value[1][1], pipeline)
+        pipelines = compose_pipeline(value[0]['pipeline'], value[1]['pipeline'], pipeline)
         result = -1
-        if conf1 > conf2:
+        if value[0]['accuracy'] > value[1]['accuracy']:
             result = 1
-        elif conf1 == conf2:
+        elif value[0]['accuracy'] == value[1]['accuracy']:
             result = 0
-        elif conf1 < conf2:
+        elif value[0]['accuracy'] < value[1]['accuracy']:
             result = 2
         if result == -1:
             raise ValueError('A very bad thing happened.')
@@ -297,7 +301,7 @@ def aggregate_results(simple_results, pipeline, categories):
         if validity:
             grouped_by_algorithm_results, grouped_by_dataset_result = compute_result(
                 result, dataset, acronym, grouped_by_algorithm_results, grouped_by_dataset_result, pipelines, categories,
-                [value[0][4], value[1][4]], [conf1, conf2])
+                [value[0]['baseline_score'], value[1]['baseline_score']], [value[0]['accuracy'], value[1]['accuracy']])
         else:
             grouped_by_dataset_result[dataset][acronym] = problem
             grouped_by_algorithm_results[acronym][problem] += 1
@@ -311,7 +315,7 @@ def create_summary(grouped_by_algorithm_results, categories):
     return summary
 
 def write_summary(result_path, grouped_by_algorithm_results, summary):
-    with open(os.path.join(result_path, 'results.csv'), "a") as out:
+    with open(os.path.join(result_path, 'results.csv'), "w") as out:
         out.write(',' + ','.join(summary.keys()) + '\n')
         for key, value in grouped_by_algorithm_results.items():
             row = key
