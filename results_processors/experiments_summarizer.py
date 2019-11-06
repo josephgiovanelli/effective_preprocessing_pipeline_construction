fa9348cdafa361d38874831925a5cc96595c5b11
Joseph Giovanelli <joseph.giovanelli@studio.unibo.it>
2019-11-06 16:21:14 +0100

    Reorganize folder results

--- results_processors/experiments_summarizer.py
+++ results_processors/experiments_summarizer.py
@@ -2,6 +2,8 @@ from __future__ import print_function
 
 import argparse
 
+import os
+
 from results_processors.correlation_utils import create_num_equal_elements_matrix, save_num_equal_elements_matrix, \
     create_correlation_matrix, save_correlation_matrix, chi2test, chi2tests, save_chi2tests
 from results_processors.results_mining_utils import create_possible_categories, get_filtered_datasets, load_results, \
@@ -16,27 +18,36 @@ def parse_args():
     args = parser.parse_args()
     return args.input, args.output, args.pipeline
 
+def create_directory(result_path, directory):
+    result_path = os.path.join(result_path, directory)
+
+    if not os.path.exists(result_path):
+        os.makedirs(result_path)
+
+    return result_path
+
 def main():
     input_path, result_path, pipeline = parse_args()
     categories = create_possible_categories(pipeline)
+    result_path = create_directory(result_path, 'summary')
 
     filtered_datasets = get_filtered_datasets()
 
     simple_results = load_results(input_path, filtered_datasets)
-    save_simple_results(result_path, simple_results, filtered_datasets)
+    save_simple_results(create_directory(result_path, 'algorithms_summary'), simple_results, filtered_datasets)
 
     grouped_by_algorithm_results, grouped_by_dataset_result = aggregate_results(simple_results, pipeline, categories)
     summary = compute_summary(grouped_by_algorithm_results, categories)
     save_grouped_by_algorithm_results(result_path, grouped_by_algorithm_results, summary)
 
     test, order_test, not_order_test = chi2tests(grouped_by_algorithm_results, summary, categories)
-    save_chi2tests(result_path, test, order_test, not_order_test)
+    save_chi2tests(create_directory(result_path, 'chi2tests'), test, order_test, not_order_test)
 
     num_equal_elements_matrix = create_num_equal_elements_matrix(grouped_by_dataset_result)
-    save_num_equal_elements_matrix(result_path, num_equal_elements_matrix)
+    save_num_equal_elements_matrix(create_directory(result_path, 'correlations'), num_equal_elements_matrix)
 
     for consider_just_the_order in [True, False]:
         correlation_matrix = create_correlation_matrix(filtered_datasets, grouped_by_dataset_result, categories, consider_just_the_order)
-        save_correlation_matrix(result_path, correlation_matrix, consider_just_the_order)
+        save_correlation_matrix(create_directory(result_path, 'correlations'), correlation_matrix, consider_just_the_order)
 
 main()
