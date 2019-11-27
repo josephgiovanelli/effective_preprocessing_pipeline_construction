3fa9f3d6c7bc57b3c1eb6790bbe3f8fb6ed1c3d0
Joseph Giovanelli <joseph.giovanelli@studio.unibo.it>
2019-11-27 16:19:03 +0100

    Redesign meta-features extractor and join

--- results_processors/meta_features/processors/meta_features_extractor.py
+++ results_processors/meta_features/processors/meta_features_extractor.py
@@ -1,8 +1,14 @@
 import openml
 import pandas as pd
 from pymfe.mfe import MFE
+from sklearn.impute import SimpleImputer
 
-from commons import benchmark_suite
+
+benchmark_suite = [3, 6, 11, 12, 14, 15, 16, 18, 22, 23, 28, 29, 31, 32, 37, 44, 46, 50, 54, 151, 182, 188, 38, 307,
+                       300, 458, 469, 554, 1049, 1050, 1053, 1063, 1067, 1068, 1590, 4134, 1510, 1489, 1494, 1497, 1501,
+                       1480, 1485, 1486, 1487, 1468, 1475, 1462, 1464, 4534, 6332, 1461, 4538, 1478, 23381, 40499,
+                       40668, 40966, 40982, 40994, 40983, 40975, 40984, 40979, 40996, 41027, 23517, 40923, 40927, 40978,
+                       40670, 40701]
 
 def get_filtered_datasets():
     df = pd.read_csv("../meta_features/simple-meta-features.csv")
@@ -13,28 +19,36 @@ def get_filtered_datasets():
     df = df['did']
     return df.values.flatten().tolist()
 
-meta_features = []
-for id in get_filtered_datasets():
-    dataset = openml.datasets.get_dataset(id)
-    X, y, _, _ = dataset.get_data(
-        dataset_format='array',
-        target=dataset.default_target_attribute)
-    dict = {'id': id}
-    print(id, dataset.name)
+for impute in [True, False]:
+    for general in [True, False]:
 
-    mfe = MFE()
-    #mfe = MFE(groups=["model-based", "landmarking"])
-    #mfe = MFE(groups=["general", "statistical", "info-theory"])
-    mfe.fit(X, y)
-    ft = mfe.extract()
+        meta_features = []
+        for id in get_filtered_datasets():
+            dataset = openml.datasets.get_dataset(id)
+            X, y, _, _ = dataset.get_data(
+                dataset_format='array',
+                target=dataset.default_target_attribute)
 
-    for i in range(0, len(ft[0])):
-        dict[ft[0][i]] = ft[1][i]
+            if impute:
+                X = SimpleImputer(strategy="constant").fit_transform(X)
 
-    meta_features.append(dict)
-    print(dict)
+            dict = {'id': id}
+            print(id, dataset.name)
 
-df = pd.DataFrame(meta_features)
-df.to_csv('../meta_features/extracted-meta-features.csv', index=False)
-#df.to_csv('../meta_features/extracted-meta-features1.csv', index=False)
-#df.to_csv('../meta_features/extracted-meta-features2.csv', index=False)
+            if general:
+                mfe = MFE(groups=["general", "statistical", "info-theory"])
+            else:
+                mfe = MFE(groups=["model-based", "landmarking"])
+
+            mfe.fit(X, y)
+            ft = mfe.extract()
+
+            for i in range(0, len(ft[0])):
+                dict[ft[0][i]] = ft[1][i]
+
+            meta_features.append(dict)
+
+        df = pd.DataFrame(meta_features)
+
+        name = ('imputed-' if impute else '') + 'extracted-meta-features' + ('-general' if general else 'model-landmarking')
+        df.to_csv('../meta_features/' + name + '.csv', index=False)
--- results_processors/meta_features/processors/meta_features_joiner.py
+++ results_processors/meta_features/processors/meta_features_joiner.py
@@ -1,21 +1,8 @@
-from scipy.stats import chi2_contingency, chi2
-from sklearn.compose import ColumnTransformer
-from sklearn.pipeline import Pipeline
-from yellowbrick.target import FeatureCorrelation
-from sklearn.preprocessing import OrdinalEncoder, LabelEncoder, FunctionTransformer
-
-from scipy import stats as s
-
-import os
-
-import numpy as np
 import pandas as pd
 
-from commons import algorithms
-
-def join_meta_features():
-    meta = pd.read_csv('../meta_features/extracted-meta-features1.csv')
-    meta2 = pd.read_csv('../meta_features/extracted-meta-features2.csv')
+def join_meta_features(impute):
+    meta = pd.read_csv('../meta_features/' + ('imputed-' if impute else '') + 'extracted-meta-features-general.csv')
+    meta2 = pd.read_csv('../meta_features/' + ('imputed-' if impute else '') + 'extracted-meta-features-model-landmarking.csv')
 
     join = pd.merge(meta2, meta, left_on='id', right_on='id')
 
@@ -25,6 +12,6 @@ def save_data_frame(result_path, data_frame, index):
     data_frame.to_csv(result_path, index=index)
 
 
-
-data = join_meta_features()
-save_data_frame('../meta_features/extracted-meta-features.csv', data, index=False)
+for impute in [True, False]:
+    data = join_meta_features(impute)
+    save_data_frame('../meta_features/' + ('imputed-' if impute else '') + 'extracted-meta-features.csv', data, index=False)
