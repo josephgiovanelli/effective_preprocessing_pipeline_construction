cab742f4708b2e2010976073c795dc21e376ab35
Joseph Giovanelli <joseph.giovanelli@studio.unibo.it>
2019-11-28 15:10:17 +0100

    Modify meta_fetures_joiner

--- results_processors/meta_features/processors/meta_features_joiner.py
+++ results_processors/meta_features/processors/meta_features_joiner.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 def join_meta_features(impute):
-    meta = pd.read_csv('../meta_features/' + ('imputed-' if impute else '') + 'extracted-meta-features-general.csv')
-    meta2 = pd.read_csv('../meta_features/' + ('imputed-' if impute else '') + 'extracted-meta-features-model-landmarking.csv')
+    meta = pd.read_csv('../' + ('imputed-mean-' if impute else '') + 'extracted-meta-features-general.csv')
+    meta2 = pd.read_csv('../' + ('imputed-mean-' if impute else '') + 'extracted-meta-features-model-landmarking.csv')
 
-    join = pd.merge(meta2, meta, left_on='id', right_on='id')
+    join = pd.merge(meta, meta2, left_on='id', right_on='id')
 
     return join
 
@@ -14,4 +14,4 @@ def save_data_frame(result_path, data_frame, index):
 
 for impute in [True, False]:
     data = join_meta_features(impute)
-    save_data_frame('../meta_features/' + ('imputed-' if impute else '') + 'extracted-meta-features.csv', data, index=False)
+    save_data_frame('../' + ('imputed-mean-' if impute else '') + 'extracted-meta-features.csv', data, index=False)
