6f198af5e9ce39862ea4752d37d713ba69b5119e
Joseph Giovanelli <joseph.giovanelli@studio.unibo.it>
2021-02-17 15:01:05 +0100

    Add extended_benchmark_suite to results_processors

--- results_processors/results_extraction_utils.py
+++ results_processors/results_extraction_utils.py
@@ -13,6 +13,9 @@ benchmark_suite = [3, 6, 11, 12, 14, 15, 16, 18, 22, 23, 28, 29, 31, 32, 37, 44,
                        1480, 1485, 1486, 1487, 1468, 1475, 1462, 1464, 4534, 6332, 1461, 4538, 1478, 23381, 40499,
                        40668, 40966, 40982, 40994, 40983, 40975, 40984, 40979, 40996, 41027, 23517, 40923, 40927, 40978,
                        40670, 40701]
+extended_benchmark_suite = [41145, 41156, 41157, 4541, 41158, 42742, 40498, 42734, 41162, 42733, 42732, 1596, 40981, 40685, 
+                        4135, 41142, 41161, 41159, 41163, 41164, 41138, 41143, 41146, 41150, 40900, 41165, 41166, 41168, 41169, 
+                        41147, 1111, 1169, 41167, 41144, 1515, 1457, 181]
 
 
 def create_possible_categories(pipeline):
@@ -40,7 +43,7 @@ def create_possible_categories(pipeline):
 
 def get_filtered_datasets():
     df = pd.read_csv('results_processors/meta_features/simple-meta-features.csv')
-    df = df.loc[df['did'].isin(benchmark_suite)]
+    df = df.loc[df['did'].isin(extended_benchmark_suite)]
     df = df.loc[df['NumberOfMissingValues'] / (df['NumberOfInstances'] * df['NumberOfFeatures']) < 0.1]
     df = df.loc[df['NumberOfInstancesWithMissingValues'] / df['NumberOfInstances'] < 0.1]
     df = df.loc[df['NumberOfInstances'] * df['NumberOfFeatures'] < 5000000]
