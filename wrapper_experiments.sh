45e83dd99404c10c3dc27aaafb4d9514339ddf3e
Joseph Giovanelli <joseph.giovanelli@studio.unibo.it>
2021-02-23 16:08:36 +0100

    Update wrapper_experiments.sh

--- wrapper_experiments.sh
+++ wrapper_experiments.sh
@@ -10,12 +10,12 @@ python experiments_launcher.py -p discretize features -r results/pipeline_constr
 python experiments_launcher.py -p features discretize -r results/pipeline_construction/discretize_features/conf2
 python results_processors/experiments_summarizer.py -p discretize features -i results/pipeline_construction/discretize_features/ -o results/pipeline_construction/discretize_features/
 
-python experiments_launcher.py -p features normalizer -r results/pipeline_construction/features_normalizer/conf1
-python experiments_launcher.py -p normalizer features -r results/pipeline_construction/features_normalizer/conf2
-python results_processors/experiments_summarizer.py -p features normalizer -i results/pipeline_construction/features_normalizer/ -o results/pipeline_construction/features_normalizer/
+python experiments_launcher.py -p features normalize -r results/pipeline_construction/features_normalize/conf1
+python experiments_launcher.py -p normalize features -r results/pipeline_construction/features_normalize/conf2
+python results_processors/experiments_summarizer.py -p features normalize -i results/pipeline_construction/features_normalize/ -o results/pipeline_construction/features_normalize/
 
 python experiments_launcher.py -p discretize rebalance -r results/pipeline_construction/discretize_rebalance/conf1
-python experiments_launcher.py -p rebalance discretize -r results/pipeline_construction/iscretize_rebalance/conf2
+python experiments_launcher.py -p rebalance discretize -r results/pipeline_construction/discretize_rebalance/conf2
 python results_processors/experiments_summarizer.py -p discretize rebalance -i results/pipeline_construction/discretize_rebalance/ -o results/pipeline_construction/discretize_rebalance/
 
 python results_processors/graphs_maker.py
